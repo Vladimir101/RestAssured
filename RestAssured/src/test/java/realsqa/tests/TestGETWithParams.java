9b27c0b352451fc472a285564e1a86470084b3e6
Vladimir101 <vladimirbelor100@gmail.com>
2021-05-20 22:03:55 -0700

    minor

--- RestAssured/src/test/java/realsqa/tests/TestGETWithParams.java
+++ RestAssured/src/test/java/realsqa/tests/TestGETWithParams.java
@@ -1,23 +1,25 @@
 package realsqa.tests;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 import org.junit.jupiter.api.Test;
 
-import realsqa.common.BaseTest;
+// https://jsonplaceholder.typicode.com/comments?postId=6
 
-class TestGETWithParams extends BaseTest
+class TestGETWithParams 
 {
 	@Test
 	void test()
 	{
 		given().
-			baseUri("https://jsonplaceholder.typicode.com/").
+			baseUri("https://jsonplaceholder.typicode.com").
 			basePath("/comments").
 			param("postId", 6).
 		when().
 			get().
 		then().
-			body("", hasSize(5));
+			body("", hasSize(5)).
+			log().body();
 	}
 }
